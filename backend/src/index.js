a10c0efd890eadc4db430db9dd960f42cc481dcb
mahtech149 <mahtech149@gmail.com>
2026-01-02 12:57:08 -0800

    Fix Express wildcard route error

--- backend/src/index.js
+++ backend/src/index.js
@@ -38,12 +38,13 @@ app.use(cors(corsOptions));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === "production"){
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
   });
+
 }
 const PORT = process.env.PORT || 5001;
 
